436ed7ca7973ff92660027126311271d228f81c8
Ira-Ivaa <iravi532@gmail.com>
2026-03-18 11:14:42 +0300

    done

--- script.js
+++ script.js
@@ -12,47 +12,153 @@ const userName = document.getElementById("username");
 const postsList = document.getElementById("posts-list");
 
 // Показать разные экраны
-function showWelcome() {}
+function showWelcome() {
+    hideAll();
+    welcomeScreen.style.display = "block";
+}
 
-function showRegister() {}
+function showRegister() {
+    hideAll();
+    registerForm.style.display = "block";
+}
 
-function showLogin() {}
+function showLogin() {
+    hideAll();
+    loginForm.style.display = "block";
+}
 
-function hideAll() {}
+function hideAll() {
+    welcomeScreen.style.display = "none";
+    registerForm.style.display = "none";
+    loginForm.style.display = "none";
+    content.style.display = "none";
+}
 
 // Получить пользователей
-function getUsers() {}
+function getUsers() {
+    const data = localStorage.getItem("users");
+    return data ? JSON.parse(data) : [];
+}
 
 // Сохранить пользователей
-function saveUsers(users) {}
+function saveUsers(users) {
+    localStorage.setItem("users", JSON.stringify(users));
+}
 
 // Генерация токена (имитация JWT)
-function generateToken(username) {}
+function generateToken(username) {
+    return "jwt_" + btoa(username + Date.now());
+}
 
 // Регистрация нового пользователя
-function register() {}
+function register() {
+    const username = inputName.value.trim();
+    const password = inputPass.value.trim();
+
+    if (!username || !password) {
+        alert("Заполни все поля");
+        return;
+    }
+
+    const users = getUsers();
+    if (users.find((u) => u.username === username)) {
+        alert("Этот логин уже используется");
+        return;
+    }
+
+    users.push({ username, password });
+    saveUsers(users);
+
+    console.log("=== РЕГИСТРАЦИЯ ===");
+    console.log("Новый пользователь:", { username });
+
+    alert("Пользователь успешно зарегестрировался");
+    showLogin();
+}
 
 // Вход пользователя
-function login() {}
+function login() {
+    const username = loginName.value.trim();
+    const password = loginPass.value.trim();
+
+    if (!username || !password) {
+        alert("Заполни все поля");
+        return;
+    }
+
+    const users = getUsers();
+    const user = users.find((u) => {
+        console.log(u.username, u.password);
+        return u.username === username && u.password === password;
+    });
+
+    if (!user) {
+        alert("Неверный логин или пароль");
+        console.warn("Попытка входа с ошибкой:", { username });
+        return;
+    }
+
+    const token = generateToken(username);
+    localStorage.setItem("token", token);
+    localStorage.setItem("currentUser", username);
+
+    console.log("=== АУТЕНТИФИКАЦИЯ УСПЕШНА ===");
+    console.log("Токен:", token);
+
+    showProtectedContent();
+}
 
 // Выход из аккаунта
-function logout() {}
+function logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("currentUser");
+    showWelcome();
+}
 
 // Показать защищённый контент
 async function showProtectedContent() {
-  // Имитация защищённого API
-  try {
-  } catch (err) {
-    console.error("Ошибка загрузки:", err);
-    alert("Не удалось загрузить данные");
-  }
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("currentUser");
+
+    if (!token || !username) {
+        alert("Доступ запрещен");
+        showWelcome();
+        return;
+    }
+    // Имитация защищённого API
+    try {
+        const postResponse = await fetch(
+            "https://jsonplaceholder.typicode.com/posts?userId=1"
+        );
+        console.log("=== ЗАПРОС К API ===");
+        console.log("Статус:", postResponse.status);
+
+        const posts = await postResponse.json();
+
+        userName.textContent = username;
+        postsList.textContent - "";
+
+        posts.slice(0, 5).forEach((post) => {
+            const li = document.createElement("li");
+            li.textContent = post.title;
+            postsList.appendChild(li);
+        });
+
+        hideAll();
+        content.style.display = "block";
+    } catch (err) {
+        console.error("Ошибка загрузки:", err);
+        alert("Не удалось загрузить данные");
+    }
 }
 
 // При загрузке страницы
 window.onload = () => {
-  if (localStorage.getItem("token") && localStorage.getItem("currentUser")) {
-    showProtectedContent();
-  } else {
-    showWelcome();
-  }
+    if (localStorage.getItem("token") && localStorage.getItem("currentUser")) {
+        showProtectedContent();
+    } else {
+        showWelcome();
+    }
 };
+
+// localStorage.clear();
